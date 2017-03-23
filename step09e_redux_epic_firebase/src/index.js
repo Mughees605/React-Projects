aa97ae818ea3dafbf7b94acd2115da4d3d22435f
Hafiz Mughees <mughees605@gmail.com>
2017-03-22 18:49:45 -0700

    whitespace

--- step09e_redux_epic_firebase/src/index.js
+++ step09e_redux_epic_firebase/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Main from "./container/main"
-import {Provider} from "react-redux";
-import {TodoAction} from "./store/actions"
+import { Provider } from "react-redux";
+import { TodoAction } from "./store/actions"
 var store = require("./store/store.js").storeConfig();
 ReactDOM.render(
- <Provider store = {store}>
-   <Main/>
- </Provider>
- ,
+  <Provider store={store}>
+    <Main />
+  </Provider>
+  ,
   document.getElementById('root')
 );
