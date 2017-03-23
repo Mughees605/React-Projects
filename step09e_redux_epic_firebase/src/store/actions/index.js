02f7f5433d857ccb62bf5d1435765ae2feaac4e7
Hafiz Mughees <mughees605@gmail.com>
2017-03-22 18:49:18 -0700

    whitespace

--- step09e_redux_epic_firebase/src/store/actions/index.js
+++ step09e_redux_epic_firebase/src/store/actions/index.js
@@ -3,12 +3,12 @@ export class TodoAction {
     static GET_TODO = "GET_TODO";
     static TODO_NULL = "TODO_NULL";
     static GET_TODO_ADDED = "GET_TODO_ADDED";
-  
+
     static addTodo = (todo) => {
-        return {type: TodoAction.ADD_TODO,payload:todo}
+        return { type: TodoAction.ADD_TODO, payload: todo }
     }
 
-    static getTodo = ()=>{
-        return { type:TodoAction.GET_TODO}
+    static getTodo = () => {
+        return { type: TodoAction.GET_TODO }
     }
 }
