81efa9182e452591a1e735974ab7a7eaba5a3d21
mughees605 <mughees605@gmail.com>
2017-03-24 18:40:26 +0500

    white space

--- step09e_redux_epic_firebase/src/store/reducer/index.js
+++ step09e_redux_epic_firebase/src/store/reducer/index.js
@@ -1,16 +1,16 @@
 import { TodoAction } from "../actions"
 
-const INITIAL_STATE = ["text"] 
+const INITIAL_STATE = ["text"]
 
 
-export var TodoReducer = (state=INITIAL_STATE, action) => {
+export var TodoReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case TodoAction.GET_TODO_ADDED:
             return {
                 ...state,
                 payload: action.payload.parsedTodo
             }
-            case TodoAction.DELETE_TODO:
+        case TodoAction.DELETE_TODO:
             return state
         default:
             return state;
--- step09e_redux_epic_firebase/src/store/store.js
+++ step09e_redux_epic_firebase/src/store/store.js
@@ -1,23 +1,23 @@
 import Redux from "redux";
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import {TodoReducer} from "./reducer/index.js";
-import {combineEpics, createEpicMiddleware} from 'redux-observable';
-import {todoEpic} from "./epic/epic.js"
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { TodoReducer } from "./reducer/index.js";
+import { combineEpics, createEpicMiddleware } from 'redux-observable';
+import { todoEpic } from "./epic/epic.js"
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 
 export var storeConfig = function () {
-    const rootEpic = combineEpics(todoEpic.addTodo,todoEpic.getTodos,todoEpic.deleteTodo);
+    const rootEpic = combineEpics(todoEpic.addTodo, todoEpic.getTodos, todoEpic.deleteTodo);
 
     const epicMiddleware = createEpicMiddleware(rootEpic);
-    
-    var reducer = combineReducers({TodoReducer});
 
-    const createStoreWithMiddleware = applyMiddleware(epicMiddleware,logger())(createStore)
+    var reducer = combineReducers({ TodoReducer });
+
+    const createStoreWithMiddleware = applyMiddleware(epicMiddleware, logger())(createStore)
 
     const middleWare = applyMiddleware(epicMiddleware);
     var store = createStoreWithMiddleware(reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
     return store;
