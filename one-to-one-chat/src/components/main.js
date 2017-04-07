568fd6e06d2804561f3f7086e7d56b3a16546eae
mughees605 <mughees605@gmail.com>
2017-04-07 17:57:12 +0500

    adding main component

---
+++ one-to-one-chat/src/components/main.js
@@ -0,0 +1,12 @@
+import React , {Component} from "react";
+
+class Main extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Main Component</h1>
+            </div>
+        )
+    }
+}
+export default Main
--- one-to-one-chat/src/index.js
+++ one-to-one-chat/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Main from "./components/main.js"
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Main />,
   document.getElementById('root')
 );
