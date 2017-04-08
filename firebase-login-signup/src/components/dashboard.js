bca759353aa387f58a38063e75a718888b1f870c
mughees605 <mughees605@gmail.com>
2017-04-08 18:17:14 +0500

    whitespace

--- firebase-login-signup/src/components/dashboard.js
+++ firebase-login-signup/src/components/dashboard.js
@@ -1,8 +1,8 @@
-import React,{Component} from "react"
+import React, {Component} from "react"
 
-class DashBoard extends Component{
-    render(){
-        return(
+class DashBoard extends Component {
+    render() {
+        return (
             <div>
                 DashBoard
             </div>
