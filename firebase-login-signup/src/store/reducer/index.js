8e4d57a7e991fbb0639a0d86c20862d1cba288d6
mughees605 <mughees605@gmail.com>
2017-04-08 17:30:30 +0500

    whitespace

--- firebase-login-signup/src/store/reducer/index.js
+++ firebase-login-signup/src/store/reducer/index.js
@@ -1,26 +1,39 @@
 import {SignUpAction} from "../action"
 import {LoginAction} from "../action/login"
 const INITIAL_STATE = {
-     isSignUp:false,
+    isSignUp: false
 }
-export var SignUpReducer = (state=INITIAL_STATE,action)=>{
+export var SignUpReducer = (state = INITIAL_STATE, action) => {
 
-    switch(action.type){
+    switch (action.type) {
         case SignUpAction.SIGN_UP_SUCESS:
-        return {...state,isSignUp:true};
+            return {
+                ...state,
+                isSignUp: true
+            };
         case SignUpAction.SIGN_UP_UNSUCESS:
-        return {...state,isSignUp:false}
+            return {
+                ...state,
+                isSignUp: false
+            }
         default:
-        return state
+            return state
     }
-    
-}
-export var LoginReducer = (state={isLogin:false,user:{}},action)=>{
 
-    switch(action.type){
-     case LoginAction.LOGIN_SUCCESS:
-     return {...state,isLogin:true,user:action.payload};
-     default:
-     return state;
+}
+export var LoginReducer = (state = {
+    isLogin: false,
+    user: {}
+}, action) => {
+
+    switch (action.type) {
+        case LoginAction.LOGIN_SUCCESS:
+            return {
+                ...state,
+                isLogin: true,
+                user: action.payload
+            };
+        default:
+            return state;
     }
 }
