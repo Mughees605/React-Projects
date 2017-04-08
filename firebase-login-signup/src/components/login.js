189be6c0ba7ae0cfa7c8843947ea66336892eb18
mughees605 <mughees605@gmail.com>
2017-04-08 16:40:56 +0500

    signup action

--- firebase-login-signup/src/components/login.js
+++ firebase-login-signup/src/components/login.js
@@ -1,17 +1,37 @@
 import React, {Component} from "react";
-import {TextField, Paper,RaisedButton} from "material-ui";
+import {TextField, Paper, RaisedButton} from "material-ui";
 
 class Login extends Component {
+    handleLogin() {
+        var credentials = {};
+        credentials.email = this
+            .refs
+            .email
+            .getValue();
+        credential.password = this
+            .refs
+            .pass
+            .getValue();
+    }
     render() {
         return (
-            <div style={{width:"400px",margin:"20px auto",textAlign:"center" ,}}>
+            <div
+                style={{
+                width: "400px",
+                margin: "20px auto",
+                textAlign: "center"
+            }}>
                 <Paper zDepth={3}>
                     <h1>Login</h1>
-                    <TextField floatingLabelText="Type your email" ref="email"  fullWidth={true}/>
+                    <TextField floatingLabelText="Type your email" ref="email" fullWidth={true}/>
                     <br/>
-                    <TextField floatingLabelText="type your password" type="password"  fullWidth={true}/>
+                    <TextField
+                        floatingLabelText="type your password"
+                        ref="password"
+                        type="password"
+                        fullWidth={true}/>
                     <br/>
-                    <RaisedButton fullWidth={true} >Login</RaisedButton>
+                    <RaisedButton fullWidth={true}>Login</RaisedButton>
                 </Paper>
             </div>
         )
---
+++ firebase-login-signup/src/store/action/login.js
@@ -0,0 +1,24 @@
+export class LoginAction{
+    static LOGIN = "LOGIN";
+    static LOGIN_SUCCESS = "LOGIN_SUCCESS";
+    static LOGIN_UN_SUCCESS = "LOGIN_UN_SUCCESS";
+
+    static login(credentials){
+        return {
+            type:LoginAction.LOGIN,
+            payload:null
+        }
+    }
+    static loginSucess(){
+        return {
+            type:LoginAction.LOGIN_SUCCESS,
+            payload:null
+        }
+    }
+    static loginUnSucess(){
+        return {
+            type:LoginAction.LOGIN_UN_SUCCESS,
+            payload:null
+        }
+    }
+}
