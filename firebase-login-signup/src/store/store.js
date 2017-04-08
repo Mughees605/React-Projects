41b60498ed49e908e1e9eaa660a51cee27cd4542
mughees605 <mughees605@gmail.com>
2017-04-08 17:23:42 +0500

    added epic login

--- firebase-login-signup/src/store/store.js
+++ firebase-login-signup/src/store/store.js
@@ -1,11 +1,27 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
+import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import thunk from "redux-thunk";
 import {createLogger} from "redux-logger"
 import {SignUpReducer} from "./reducer"
+
+import {LoginEpic} from "./epic"
 const logger = createLogger();
 
-const root = combineReducers({
+const rootEpic = combineEpics(
+    LoginEpic.login
+)
+const rootReducer = combineReducers({
      SignUpReducer
 })
-const middle = applyMiddleware(thunk,logger);
-export const store = createStore(root, middle);
+
+const epicMiddleware = createEpicMiddleware(rootEpic);
+
+const createStoreWithMiddleware = applyMiddleware(thunk,logger,epicMiddleware)(createStore);
+
+
+// const middle = applyMiddleware(thunk,logger)(epicMiddleware);
+// export const store = createStore(rootReducer, middle);
+export let store = createStoreWithMiddleware(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
