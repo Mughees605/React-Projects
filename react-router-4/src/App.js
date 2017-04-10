a2bc3c36b71b986a89d711c8c090095b409e12af
mughees605 <mughees605@gmail.com>
2017-04-10 20:43:20 +0500

    added another example

--- react-router-4/src/App.js
+++ react-router-4/src/App.js
@@ -5,68 +5,26 @@ import {
   Link
 } from 'react-router-dom'
 
-const BasicExample = () => (
+const ParamsExample = () => (
   <Router>
     <div>
+      <h2>Accounts</h2>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
+        <li><Link to="/netflix">Netflix</Link></li>
+        <li><Link to="/zillow-group">Zillow Group</Link></li>
+        <li><Link to="/yahoo">Yahoo</Link></li>
+        <li><Link to="/modus-create">Modus Create</Link></li>
       </ul>
 
-      <hr/>
-
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
+      <Route path="/:id" component={Child}/>
     </div>
   </Router>
 )
 
-const Home = () => (
+const Child = ({ match }) => (
   <div>
-    <h2>Home</h2>
+    <h3>ID: {match.params.id}</h3>
   </div>
 )
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
-
-export default BasicExample
+export default ParamsExample
