5304dd1dc5fa4762b38e62b6dd0eca6da0d5237f
Hafiz Mughees <mughees605@gmail.com>
2017-04-17 11:54:56 -0700

    adding styling

--- firebase-login-signup/src/components/dashboard.js
+++ firebase-login-signup/src/components/dashboard.js
@@ -1,17 +1,17 @@
-import React, {Component} from "react"
-import {FlatButton, AppBar, IconButton,TextField, Paper, RaisedButton} from "material-ui";
+import React, { Component } from "react"
+import { FlatButton, AppBar, IconButton, TextField, Paper, RaisedButton } from "material-ui";
 const App = () => (
-  <AppBar
-    title={<span>DashBoard</span>}
-    iconElementRight={<span><FlatButton label="SignUp" /> <FlatButton label="Login" /></span>}
-  />
+    <AppBar
+        title={<span>DashBoard</span>}
+        iconElementRight={<span><FlatButton label="SignUp" /> <FlatButton label="Login" /></span>}
+    />
 );
 class DashBoard extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
-    handleMissingData(){
+    handleMissingData() {
         var userObj = {};
         userObj.name = this.refs.name.getValue();
         userObj.age = this.refs.age.getValue();
@@ -19,48 +19,48 @@ class DashBoard extends Component {
         userObj.image = this.props.photo;
         this.props.onAdd(userObj);
     }
-    handleChange(e){
+    handleChange(e) {
         this.props.onImage(e);
     }
-    
+
     render() {
         return (
             <div>
-                <AppBar/>
-                <div style={{width:"400px",margin:"0px auto",textAlign:"center"}}>
+                <AppBar />
+                <div style={{ width: "400px", margin: "0px auto", textAlign: "center" , }}>
                     <h1>Missing Reports</h1>
-                  <Paper zDepth={3}>
-                    <div>
-                        <TextField floatingLabelText="Type missing person name" ref="name" fullWidth={true}/>
-                        <br/>
-                        <TextField
-                            floatingLabelText="type missing person age"
-                            ref="age"
-                            type="number"
-                            fullWidth={true}/>
-                        <br/>
-                        <TextField
-                            floatingLabelText="Address"
-                            ref="address"
-                            fullWidth={true}/>
-                        <br/>
-                        <TextField
-                            ref="pic"
-                            type="file"
-                            onChange={this.handleChange.bind(this)}
-                            fullWidth={true}/>
-                        <br/>
-                        {console.log(this.props.per)}
-                        <progress value={this.props.per} max='100'></progress>
-                        <RaisedButton
-                            type="submit"
-                            onClick={this.handleMissingData.bind(this)}
-                            fullWidth={true}
-                        >Submit Report</RaisedButton>
-                    </div>
-                </Paper>
+                    <Paper zDepth={3}>
+                        <div style={{padding:"20px", borderRadius:"2px", margin:"10px"}}>
+                            <TextField floatingLabelText="Type missing person name" ref="name" fullWidth={true} />
+                            <br />
+                            <TextField
+                                floatingLabelText="type missing person age"
+                                ref="age"
+                                type="number"
+                                fullWidth={true} />
+                            <br />
+                            <TextField
+                                floatingLabelText="Address"
+                                ref="address"
+                                fullWidth={true} />
+                            <br />
+                            <TextField
+                                ref="pic"
+                                type="file"
+                                onChange={this.handleChange.bind(this)}
+                                fullWidth={true} />
+                            <br />
+                            {console.log(this.props.per)}
+                            <progress value={this.props.per} max='100'></progress>
+                            <RaisedButton
+                                type="submit"
+                                onClick={this.handleMissingData.bind(this)}
+                                fullWidth={true}
+                            >Submit Report</RaisedButton>
+                        </div>
+                    </Paper>
                 </div>
-                
+
             </div>
         )
     }
--- firebase-login-signup/src/components/main.js
+++ firebase-login-signup/src/components/main.js
@@ -23,4 +23,4 @@ class Main extends Component {
         )
     }
 }
-export default connect()(Main);
+export default Main;
