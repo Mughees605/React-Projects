4c6443fce484998cd9b283769836ed45ffab3878
Hafiz Mughees <mughees605@gmail.com>
2017-05-08 20:52:18 -0700

    res

--- react-native/src/components/NavHeader/NavHeader.jsx
+++ react-native/src/components/NavHeader/NavHeader.jsx
@@ -1,54 +1,53 @@
 import React, { Component } from 'react';
-import { Navbar, FormGroup, FormControl, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, FormGroup, FormControl, Row, Col } from 'react-bootstrap';
 import './NavHeader.css';
 
 class NavHeader extends Component {
   render() {
     return (
-      <div className="nav-header-container">
-        <Navbar >
-          <Navbar.Header>
-            <Navbar.Brand>
-              <h1 className="brand-name">Pipeline</h1>
-            </Navbar.Brand>
-            <Navbar.Toggle className='toggle-bar'/>
-          </Navbar.Header>
+      <Row className="nav-header-container">
+        <Navbar>
+          <Col md={2} sm={2} xs={12} >
+            <Navbar.Header>
+              <Navbar.Brand>
+                <h1 className="brand-name">Pipeline</h1>
+              </Navbar.Brand>
+              <Navbar.Toggle className='toggle-bar' />
+            </Navbar.Header>
+          </Col>
           <Navbar.Collapse>
-            <Navbar.Form pullLeft className='searchbar-group'>
-              <FormGroup className="search-engien">
-                <img src="/assets/images/search-icon.jpg" alt="Search icon" className='search-icon' />
-                <FormControl type="text" className="search-engien" placeholder="Enter Your Search.." />
-                {/*<img src="/assets/images/notify-icon.jpg" alt="Notify icon" className='notify-icon' />*/}
-                <NavDropdown eventKey={5} id="basic-nav-dropdown" pullRight className='dropdown-bar' title={<i className="glyphicon glyphicon-bell notify-icon"></i>} noCaret>
-                 <div style={{width:"434px",height: "77px",borderRadius: "5px",border:"solid 1px #eaeaea;", padding:"20px"}}>
-                   <i className="glyphicon glyphicon-user" style={{fontSize:"40px",color:"#8dc73f"}}></i>
-                   <span>Mughees</span>
-                 </div> 
-                </NavDropdown>
-                <span className='line'></span>
-                
-              </FormGroup>
-            </Navbar.Form>
-            <nav className='profile'>
-              <span className='pgrp'>
-                <p className='username'>Connor Black</p><br />
-                <p className='user-address'>Connor@digitalfreelancer.io</p>
-              </span>
-              {/*<span className='p-image-grp'>*/}
-                <a href="" className='profile-img'><img src="https://vignette2.wikia.nocookie.net/mafiagame/images/2/23/Unknown_Person.png/revision/latest?cb=20151119092211" alt="Profile Img" className='profile-img' /></a>
-                <NavDropdown eventKey={3} id="basic-nav-dropdown" pullRight className='dropdown-bar' Caret>
-                  <MenuItem eventKey={3.1}>Action</MenuItem>
-                  <MenuItem eventKey={3.2}>Another action</MenuItem>
-                  <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                  <MenuItem divider />
-                  <MenuItem eventKey={3.4}>Separated link</MenuItem>
-                </NavDropdown>
-              {/*</span>*/}
-            </nav>
+            <Col md={6} sm={4} xs={6} className="middle-col">
+              <Navbar.Form style={{ border: "none" }}>
+                <FormGroup>
+                  <Col md={1} sm={1} xs={2}>
+                    <img src="/assets/images/search-icon.jpg" alt="Search icon" className='search-icon' />
+                  </Col>
+                  <Col md={10} sm={10} xs={10}>
+                    <FormControl type="text" placeholder="Enter your search here...." className="search-engine" />
+                  </Col>
+                </FormGroup>
+                {' '}
+              </Navbar.Form>
+            </Col>
+            <Col md={4} sm={6} xs={6} className="right-col">
 
+              <div>
+                <img src="/assets/images/notify-icon.jpg" alt="Notify icon" className='notify-icon' />
+                <span className='line'></span>
+                <div className="right-col-userDetail">
+                  <p className='username'>Connor Black</p><br />
+                  <p className='user-address'>Connor@digitalfreelancer.io</p>
+                </div>
+                <div className="right-col-profile">
+                  <a href="" className='profile-img'><img src="https://vignette2.wikia.nocookie.net/mafiagame/images/2/23/Unknown_Person.png/revision/latest?cb=20151119092211" alt="Profile Img" className='profile-img' /></a>
+                </div>
+              </div>
+            </Col>
+          </Navbar.Collapse>
+          <Navbar.Collapse>
           </Navbar.Collapse>
         </Navbar>
-      </div>
+      </Row>
 
     );
   }
